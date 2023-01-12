d4b6643ab3777447c1065e4626d166ee1069df76
srikanta9563 <110465259+srikanta9563@users.noreply.github.com>
2023-01-12 15:15:35 +0530

    Add files via upload

---
+++ OneToMany/src/main/java/com/OneToMany/Entity/Items.java
@@ -0,0 +1,45 @@
+package com.OneToMany.Entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Table
+@Entity
+
+public class Items {
+	@Id
+	@GeneratedValue
+	private int id;
+	private String itemname;
+	private double price;
+	private int quantity;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getItemname() {
+		return itemname;
+	}
+	public void setItemname(String itemname) {
+		this.itemname = itemname;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	
+	
+}
