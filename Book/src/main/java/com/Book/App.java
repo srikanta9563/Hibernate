3a96ad26778db4d6c717d0e198333176e1479a7e
srikanta9563 <110465259+srikanta9563@users.noreply.github.com>
2023-01-12 15:13:02 +0530

    Add files via upload

---
+++ Book/src/main/java/com/Book/App.java
@@ -0,0 +1,13 @@
+package com.Book;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
